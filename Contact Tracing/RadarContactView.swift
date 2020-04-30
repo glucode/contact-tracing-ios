3a8c4e98f7da0240e1dff5b279276ae8f0391fd6
Nico du Plessis <duplessis.nico@gmail.com>
2020-04-30 02:56:02 +0200

    Add radar contacts and buttons

---
+++ Contact Tracing/RadarContactView.swift
@@ -0,0 +1,22 @@
+//
+//  RadarContactView.swift
+//  Contact Tracing
+//
+//  Created by Nico du Plessis on 2020/04/30.
+//  Copyright © 2020 Glucode (Pty) Ltd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RadarContactView: UIButton {
+    
+    override func awakeFromNib() {
+
+    self.layoutIfNeeded()
+    layer.cornerRadius = self.frame.height / 2.0
+    layer.masksToBounds = true
+
+    }
+    
+}
