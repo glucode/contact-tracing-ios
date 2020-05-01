e52c8d4944ec6018ea5892ff0bb972fd3485666b
Alex de Waal <alex@glucode.com>
2020-05-01 17:48:09 +0200

    Add and link niew view on Notification screen

--- Contact Tracing/Onboarding/NotificationsOnboardingViewController.swift
+++ Contact Tracing/Onboarding/NotificationsOnboardingViewController.swift
@@ -10,11 +10,76 @@ import Foundation
 import UIKit
 
 class NotificationsOnboardingViewController: UIViewController {
+
+    @IBOutlet weak var enableNotificationsView: UIView!
+    @IBOutlet weak var notificationsNotEnabledView: UIView!
+    @IBOutlet weak var openSettingsButton: UIButton!
+    @IBOutlet weak var actionButton: PillButton!
+
+    private var notificationPermissionState: NotificationPermissionState = .notDetermined
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        determineNotificationsPermissions()
+    }
+
+    @IBAction func onOpenSettingsTap(_ sender: UIButton) {
+        UIApplication.shared.openAppSettings()
+    }
     
     @IBAction func actionButtonPressed(_ sender: Any) {
-        self.navigationController?.dismiss(animated: false, completion: {
-            
-        })
+        switch notificationPermissionState {
+        case .authorized:
+            break
+        case .notDetermined:
+            requestNotificationsPermission()
+        case .denied:
+            self.navigationController?.dismiss(animated: false, completion: nil)
+        }
     }
 }
 
+// MARK: - UI
+extension NotificationsOnboardingViewController {
+    private func updateUI() {
+        DispatchQueue.main.async {
+            self.enableNotificationsView.isHidden = true
+            self.notificationsNotEnabledView.isHidden = false
+            self.actionButton.setTitle("Continue", for: .normal)
+        }
+    }
+}
+
+// MARK: - Permissions
+extension NotificationsOnboardingViewController {
+    private func requestNotificationsPermission() {
+        NotificationManager.shared.requestAuthorisation { (granted, error) in
+            if granted {
+                // TODO: Move to next screen
+                return
+            } else {
+                self.updateUI()
+            }
+        }
+    }
+
+    private func determineNotificationsPermissions() {
+        NotificationManager.shared.authorisationStatus { (status) in
+            switch status {
+            case .authorized:
+                self.notificationPermissionState = .authorized
+            case .denied:
+                self.notificationPermissionState = .denied
+                self.updateUI()
+            case .provisional, .notDetermined:
+                break
+            @unknown default:
+                break
+            }
+        }
+    }
+}
+
+public enum NotificationPermissionState {
+    case authorized, notDetermined, denied
+}
