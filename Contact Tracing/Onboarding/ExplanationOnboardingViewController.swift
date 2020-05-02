0930d5236733507de9e5b85e8a001a683a1d430b
Alex de Waal <alex@glucode.com>
2020-05-02 13:36:30 +0200

    Add methods in place to move between steps

--- Contact Tracing/Onboarding/ExplanationOnboardingViewController.swift
+++ Contact Tracing/Onboarding/ExplanationOnboardingViewController.swift
@@ -13,20 +13,60 @@ import Lottie
 class ExplanationOnboardingViewController: UIViewController {
     
     @IBOutlet weak var animationView: AnimationView!
-    
-    
-    override func viewDidAppear(_ animated: Bool) {
-        showStepOne()
+
+    private var stepCounter = 0
+    private var _stepCounter: Int {
+        get {
+            return stepCounter
+        } set {
+            self.stepCounter = newValue
+            updateUIBasedOnStep()
+        }
+    }
+
+    @IBAction func onNextTap(_ sender: UIButton) {
+        if stepCounter < 6 {
+            self._stepCounter += 1
+        } else {
+            performSegue(withIdentifier: "onboardingStepComplete", sender: nil)
+        }
+    }
+
+    @IBAction func onBackTap(_ sender: UIButton) {
+        if stepCounter > 0 && stepCounter < 7 {
+            self._stepCounter -= 1
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+
+    private var interfaceStyle: String {
+        let style = isDarkMode ? "dark" : "light"
+        return style
     }
     
     private func showStepOne() {
-        if isDarkMode {
-            animationView.animation = Animation.named("how-works-step1-dark")
-        } else {
-            animationView.animation = Animation.named("how-works-step1-light")
-        }
-        
+        animationView.animation = Animation.named("how-works-step1-\(interfaceStyle)")
         animationView.play()
+    }
+
+    func updateUIBasedOnStep() {
+        switch stepCounter {
+        case 1:
+            showStepOne()
+        case 2:
+            print(stepCounter)
+        case 3:
+            print(stepCounter)
+        case 4:
+            print(stepCounter)
+        case 5:
+            print(stepCounter)
+        case 6:
+            print(stepCounter)
+        default:
+            break
+        }
     }
     
 }
